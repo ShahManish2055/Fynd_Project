9d900d7f214e680c0d2b4241125f4e5d09879b47
ShahManish2055 <1903101@ritindia.edu>
2023-03-16 21:40:37 +0545

    commit

---
+++ MEVN/Vue Js/firstproject/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <HelloWorld msg="Hello my name is manish shah i learning vue js"/>
+  <Hello msg1="Hello msg1"/>
+  <!-- <h2>{{msg1}}</h2> -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Task.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ MEVN/Vue Js/firstproject/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{msg1}}</h2>
+    <ul>
+      <li><a href="https://community.fynd.academy/?tab=Posts" target="_blank" rel="noopener"></a>Pensil</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // shah:'shah this is my surname',
+  props: {
+    msg: String,
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
