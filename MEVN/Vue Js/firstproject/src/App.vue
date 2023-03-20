86fbf60887b1ab446cd8bd6df7d2422780786190
ShahManish2055 <1903101@ritindia.edu>
2023-03-20 21:32:35 +0545

    commit

--- MEVN/Vue Js/firstproject/src/App.vue
+++ MEVN/Vue Js/firstproject/src/App.vue
@@ -2,18 +2,34 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
-  <HelloWorld msg="Hello my name is manish shah i learning vue js"/>
-  <Hello msg1="Hello msg1"/>
+  <!-- <HelloWorld msg="Hello my name is manish shah i learning vue js"/>
+  <Hello msg1="Hello msg1"/> -->
+  <Hello1 msg2="This is the fruits list component"/>
+  <div id="app">
+    <Hello1 />
+    <ul>
+      <li> Apple</li>
+      <li> Banana</li>
+      <li> Mango</li>
+      <li> orange</li>
+     
+    </ul>
+    
+  </div>
   <!-- <h2>{{msg1}}</h2> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Hello from './components/Task.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Hello from './components/Task.vue'
+import Hello1 from './components/TestTask2.vue'
+
+
 export default {
   name: 'App',
   components: {
-    HelloWorld,Hello
+
+    Hello1
   }
 }
 </script>
@@ -26,5 +42,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+    background-color: aqua;
+  margin-top: auto;
 }
 </style>
---
+++ MEVN/Vue Js/firstproject/src/components/TestTask2.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Hello1">
+      <h2>{{msg2}}</h2>
+      
+    </div>
+  </template>
+  
+  <script >
+  export default {
+    name: 'HelloProject',
+    props: {
+      msg2: String ,
+     
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  .Hello1 {
+    background-color:burlywood;
+    color: white;
+  }
+  
+  </style>
+  
