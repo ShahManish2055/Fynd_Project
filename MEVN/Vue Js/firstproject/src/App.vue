82bc38d97c59fabce703b4ce44a22861a103c2e6
ShahManish2055 <1903101@ritindia.edu>
2023-03-30 20:57:54 +0545

    commit

--- MEVN/Vue Js/firstproject/src/App.vue
+++ MEVN/Vue Js/firstproject/src/App.vue
@@ -1,11 +1,11 @@
-<template>
+<!-- <template> -->
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
   <!-- <HelloWorld msg="Hello my name is manish shah i learning vue js"/>
   <Hello msg1="Hello msg1"/> -->
-  <Hello1 msg2="This is the fruits list component"/>
-  <div id="app">
+  <!-- <Hello1 msg2="This is the fruits list component"/> -->
+  <!-- <div id="app">
     <Hello1 />
     <ul>
       <li> Apple</li>
@@ -15,26 +15,29 @@
      
     </ul>
     
-  </div>
+  </div> -->
   <!-- <h2>{{msg1}}</h2> -->
+  <!-- <div id="app">
+    <Main />
+  </div>
 </template>
-
-<script>
+-->
+<!-- <script> 
 // import HelloWorld from './components/HelloWorld.vue'
 // import Hello from './components/Task.vue'
-import Hello1 from './components/TestTask2.vue'
+// import Hello1 from './components/TestTask2.vue'
+// import Main from './components/MainApp.vue'
 
+// export default {
+//   name: 'App',
+//   components: {
+//     Main
+//     // Hello1
+//   }
+// }
+// </script> -->
 
-export default {
-  name: 'App',
-  components: {
-
-    Hello1
-  }
-}
-</script>
-
-<style>
+<!-- <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -44,5 +47,36 @@ export default {
   margin-top: 60px;
     background-color: aqua;
   margin-top: auto;
+}
+</style> -->
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+ul{
+  display: flex; 
+justify-content: center;
+list-style-type: none;
+background-color: azure;
+
+}
+ul li{
+  padding: 10px;
+ 
 }
 </style>
