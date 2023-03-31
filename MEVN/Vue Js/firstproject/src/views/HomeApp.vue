74c0cbf5ad318d4a2e62ba1798c788f2784c2150
ShahManish2055 <1903101@ritindia.edu>
2023-03-31 19:48:23 +0545

    commit

--- MEVN/Vue Js/firstproject/src/views/HomeApp.vue
+++ MEVN/Vue Js/firstproject/src/views/HomeApp.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <h1>Home Page</h1>
     <p>Welcome to the home page!</p>
@@ -9,6 +9,47 @@
 export default {
   name: 'HomeA'
 }
-</script>
+</script> -->
 
-  
+<template>
+  <div>
+    <form @submit.prevent="login">
+      <label>
+        Username:
+        <input type="text" v-model="username">
+      </label>
+      <label>
+        Password:
+        <input type="password" v-model="password">
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      axios.post('/login', {
+        username: this.username,
+        password: this.password
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
